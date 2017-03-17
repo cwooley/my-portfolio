13d64c92ddca4400a6d66891e067835c8d26c5d4
Charles Wooley <cwooley89@gmail.com>
2017-03-17 03:56:49 +0000

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,8 @@ puts "5 skills created"
 end
 
 1.times do |portfolio_item|
-  Portfolio.create!(    title: "Portfolio title #{portfolio_item}",
+  Portfolio.create!(   
+    title: "Portfolio title #{portfolio_item}",
     subtitle: "Angular",
     body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://placehold.it/600x400",
@@ -56,3 +57,12 @@ end
 end
 
 puts "9 portfolio items created."
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+    )
+  
+end
+
+puts "3 technologies created"
