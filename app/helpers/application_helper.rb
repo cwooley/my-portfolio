865d7f374f031cdc0edaa295f5c57ce7234bdfb1
Charles Wooley <cwooley89@gmail.com>
2017-03-24 18:20:54 +0000

    implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def login_helper(style = '')
     if current_user.is_a?(GuestUser)
-      (link_to "Sign in", new_user_session_path, class: style) + 
+      (link_to "Sign in", new_user_session_path, class: style) + " " +
       (link_to "Register", new_user_registration_path, class: style)
     else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
@@ -21,5 +21,44 @@ module ApplicationHelper
   
   def copyright_generator()
     MammothViewTool::Renderer.copyright 'Charles Wooley', 'All Rights Reserved'
+  end
+  
+  def nav_items
+    [
+      {
+        path: root_path,
+        title: 'Home'
+      },
+      {
+        path: about_path,
+        title: 'About'
+      },
+      {
+        path: contact_path,
+        title: 'Contact'
+      },
+      {
+        path: blogs_path,
+        title: 'Blog'
+      },
+      {
+        path: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+  
+  
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:path]}'class='#{style} #{active? item[:path]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    
+    nav_links.html_safe
+  end
+  
+  def active? path
+    "active" if current_page? path
   end
 end
