57989b4fd74cdb896dd5ff8c8e27d231d49e78c7
Charles Wooley <cwooley89@gmail.com>
2017-03-28 16:22:20 +0000

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,4 +61,17 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+  
+  
+  
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+       alert_generator
+    end
+  end
+  
+  def alert_generator msg
+    js add_gritter(msg, :title => "Charles Wooley Portfolio", sticky: false)
+  end
 end
